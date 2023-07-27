f421fdf0d799fbd6e04087cdf0cb21c498f9ecd8
sufiyan <sufiyanmohd345@gmail.com>
2023-07-27 16:31:48 +0530

    firstName

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path = require("path");
+var app = express();
+var server = app.listen(3000, function(){
+  console.log("server runing...")
+});
+
+const io  = require('socket.io')(server, {
+  allowEIO3 :true,
+});
+app.use(express.static(path.join(__dirname,"")));
+  var userConnections = [];
+ io.on('connection', (socket) => {
+  console.log('socket id is', socket.id);
+  socket.on("userconnect", (data) => {
+    console.log("userconnect", data.displayName, data.meetingid);
+  var other_users = userConnections.filter(
+    (v) =>  v.meeting_id == data.meetingid
+  );
+    userConnections.push({
+       connectionId : socket.id,
+       user_id : data.displayName,
+       meeting_id : data.meetingid,
+    })
+
+    other_users.forEach((v) =>{
+      socket.to(v.connectionId).emit('inform_others_about_me', {
+        other_user_id : data.displayName,
+        connId : socket.id
+      })
+    })
+    socket.emit("infrom_me_about_other_user", other_users)
+  })
+  socket.on("SDPProcess", (data) => {
+    socket.to(data.to_connid).emit("SDPProcess", {
+      message : data.message,
+      from_connid : socket.id
+    })
+  })
+
+  socket.on("disconnect", function (){
+    console.log("disconnect");
+   var disUser =  userConnections.find((p) => p.connectionId == socket.id);
+   if(disUser){
+    var meetingid = disUser.meeting_id;
+    userConnections =  userConnections.filter((p) => p.connectionId != socket.id);
+    var list = userConnections.filter((p) => p.meeting_id == meetingid);
+    list.forEach((v) => {
+      socket.to(v.connectionId).emit("inform_other_about_disconnected_user", {
+        connId : socket.id,
+      }); 
+    })
+   }
+  })
+
+ })
+ 
